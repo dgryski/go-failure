c48294fdf99d45509544b15c3aeabb6a8c956c96
Damian Gryski <damian@gryski.com>
2014-03-05 10:00:03 +0100

    docs++

--- failure.go
+++ failure.go
@@ -1,7 +1,12 @@
 // Package failure implements the Phi Accrual Failure Detector
 /*
 
-http://ddg.jaist.ac.jp/pub/HDY+04.pdf
+Please see http://ddg.jaist.ac.jp/pub/HDY+04.pdf
+
+To use the failure detection algorithm, you need a heartbeat loop that will
+call Ping() at regular intervals.  At any point, you can call Phi() which will
+report how suspicious it is that a heartbeat has not been heard since the last
+time Ping() was called.
 
 */
 package failure
@@ -13,12 +18,16 @@ import (
 	"github.com/dgryski/go-onlinestats"
 )
 
+// Detector is a failure detector
 type Detector struct {
 	w          *onlinestats.Windowed
 	last       time.Time
 	minSamples int
 }
 
+// New returns a new failure detector that considers the last windowSize
+// samples, and ensures there are at least minSamples in the window before
+// returning an answer
 func New(windowSize, minSamples int) *Detector {
 
 	d := &Detector{
@@ -29,6 +38,7 @@ func New(windowSize, minSamples int) *Detector {
 	return d
 }
 
+// Ping registers a heart-beat at time now
 func (d *Detector) Ping(now time.Time) {
 	if !d.last.IsZero() {
 		d.w.Push(now.Sub(d.last).Seconds())
@@ -36,6 +46,7 @@ func (d *Detector) Ping(now time.Time) {
 	d.last = now
 }
 
+// Phi calculates the suspicion level at time 'now' that the remote end has failed
 func (d *Detector) Phi(now time.Time) float64 {
 	if d.w.Len() < d.minSamples {
 		return 0
@@ -48,6 +59,8 @@ func (d *Detector) Phi(now time.Time) float64 {
 	return phi
 }
 
+// cdf is the cumulative distribution function of a normally distributed random
+// variable with the given mean and standard deviation
 func cdf(mean, stddev, x float64) float64 {
 	return 0.5 + 0.5*math.Erf((x-mean)/(stddev*math.Sqrt2))
 }
